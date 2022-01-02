3ddb4253cfc0e42e17bfff5ed322f7a5efd821fe
zarrai saif edine <zarraisaifedine@gmail.com>
2022-01-03 00:07:00 +0100

    basic login/signup views

--- face_detect/models.py
+++ face_detect/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+class Profile(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=False)
+    mobile = models.CharField(max_length=15)
+    address = models.CharField(max_length=25)
+    image = models.FileField(null=True)
 
-# Create your models here.
+    def __str__(self):
+        return self.user.username
--- face_detect/views.py
+++ face_detect/views.py
@@ -1,13 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,logout
+from django.contrib.auth import login as LOGIN
+from django.contrib.auth import logout as LOGOUT
+from django.contrib.auth.models import User
+from .models import Profile
+from django.contrib.auth import get_user_model
+from django.contrib import messages
 
 def login(request):
-    pass
+    if request.method=="POST":
+        u = request.POST['uname']
+        p = request.POST['pwd']
+        user = authenticate(username=u,password=p)
+        LOGIN(request,user)
+        return redirect('home')
+    
+    return render(request,'login.html')
 
 def logout(request):
-    pass
+    LOGOUT(request)
+    return redirect('home')
 
 def signup(request):
-    pass
+
+    if request.method=="POST":
+        u = request.POST['uname']
+        p = request.POST['pwd']
+        f = request.POST['fname']
+        l = request.POST['lname']
+        e = request.POST['email']
+        a = request.POST['add']
+        m = request.POST['mobile']
+        i = request.FILES['image']
+
+
+        UserModel = get_user_model()
+        if not UserModel.objects.filter(username=u).exists():
+            user = User.objects.create_user(username=u,password=p,email=e,first_name=f,last_name=l)
+            Profile.objects.create(user=user,mobile=m,address=a,image=i)
+        else:
+            return render(request,'signup.html',{'error':'user already exist'})
+    return render(request,'signup.html')
+
 
 def home(request):
     return render(request,'home.html')
