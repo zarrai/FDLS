da12d928bad903b4b2537bfe11d6e192db491794
zarrai saif edine <zarraisaifedine@gmail.com>
2022-01-02 22:30:59 +0100

    configure project

--- face_detect/urls.py
+++ face_detect/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-
+from .views import *
 
 urlpatterns = [
     path('', home,name='home'),
-    path('login', Login,name='login'),
-    path('logout', Logout,name='logout'),
+    path('login', login,name='login'),
+    path('logout', logout,name='logout'),
     path('signup', signup,name='signup'),
-    path('about', about,name='about'),
-    path('contact', contact,name='contact'),
 ]
--- face_detect/views.py
+++ face_detect/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def login(request):
+    pass
+
+def logout(request):
+    pass
+
+def signup(request):
+    pass
+
+def home(request):
+    return render(request,'home.html')
